cb3030bf71ec2ff97bce77ead5440ac9bab2f999
sy1122young <stephen1122young@hotmail.com>
2021-10-06 14:48:14 +1300

    depricated test files, added gitignore

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ ini_set("error_reporting", E_ALL);
 ini_set("log_errors", "1");
 ini_set("error_log", "php_errors.txt");
 
-echo phpinfo();
 
 if (file_exists('xml/song_list.xml')) {
     $song_list = simplexml_load_file('xml/song_list.xml');
