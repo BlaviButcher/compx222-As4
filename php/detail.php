0e10a4cc5cfbf146108aba18b867c5bbf135e2cb
sy1122young <stephen1122young@hotmail.com>
2021-10-12 14:33:06 +1300

    :construction: base php design complete

---
+++ php/detail.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/detail.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="card">
+        <div class='img-wrap'>
+            <img src='../images/AM.jpg' alt=''>
+        </div>
+        <div class='info-wrap'>
+            <div class='field-1'><strong>Title: </strong><span>asdghsda sdfagh sdgaw dsg sdagsdag asdgsda gasdg sdag
+                    sdag sdag sadgsdgsd adsg sdgsadfghsad gs</span></div>
+            <div class='field-2'><strong>Artist: </strong><span>asdghsda sdfagh sdgaw dsg sdagsdag asdgsda gasdg sdag
+                    sdag sdag sadgsdgsd adsg sdgsadfghsad gst</span></div>
+            <div class='field-3'><strong>Album: </strong><span>asdghsda sdfagh sdgaw dsg sdagsdag asdgsda gasdg sdag
+                    sdag sdag sadgsdgsd adsg sdgsadfghsad gs</span></div>
+            <div class='field-4'><strong>Year: </strong><span>asdghsda sdfagh sdgaw dsg sdagsdag asdgsda gasdg sdag
+                    sdag sdag sadgsdgsd adsg sdgsadfghsad gs</span></div>
+            <div class='field-5'><strong>Genre: </strong><span>asdghsda sdfagh sdgaw dsg sdagsdag asdgsda gasdg sdag
+                    sdag sdag sadgsdgsd adsg sdgsadfghsad gs</span></div>
+        </div>
+    </div>
+</body>
+
+</html>
